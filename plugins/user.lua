cbe1c23cb51a69bdd268bb8dfa193cd57b0bf44f
Mahmoud Albaiyoumi <mahmoud.albaiyoumi@gs1eg.org>
2023-12-27 13:33:22 +0200

    initial commit

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,5 +9,36 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  {'akinsho/git-conflict.nvim', version = "*", config = true}
+  { "akinsho/git-conflict.nvim", version = "*", config = true, lazy = false },
+  {
+    "aaronhallaert/advanced-git-search.nvim",
+    config = function()
+      -- optional: setup telescope before loading the extension
+      require("telescope").setup {
+        -- move this to the place where you call the telescope setup function
+        extensions = {
+          advanced_git_search = {
+            -- See Config
+          },
+        },
+      }
+
+      require("telescope").load_extension "advanced_git_search"
+    end,
+    dependencies = {
+      --- See dependencies
+    },
+    {
+      "NeogitOrg/neogit",
+      dependencies = {
+        "nvim-lua/plenary.nvim", -- required
+        "sindrets/diffview.nvim", -- optional - Diff integration
+
+        -- Only one of these is needed, not both.
+        "nvim-telescope/telescope.nvim", -- optional
+        "ibhagwan/fzf-lua", -- optional
+      },
+      config = true,
+    },
+  },
 }
